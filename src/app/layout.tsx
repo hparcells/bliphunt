d24ee8e236e7430dddd6172ef79f32d4b9a8e20f
Hunter Parcells <hunterparcells@gmail.com>
2023-02-16 18:53:51 -0500

    Base app using Next.js and Turbo.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,12 @@
-import './globals.css'
+import '../styles/globals.scss';
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
+    <html lang='en'>
       <head />
       <body>{children}</body>
     </html>
-  )
+  );
 }
+
+export default Layout;
