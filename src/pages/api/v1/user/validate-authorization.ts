7b7672230a9eefe2161f6c2bafa12a09a43f490d
Hunter Parcells <hunterparcells@gmail.com>
2023-03-02 00:37:59 -0500

    Moved authorization validation to its own function.

--- src/pages/api/v1/user/validate-authorization.ts
+++ src/pages/api/v1/user/validate-authorization.ts
@@ -1,21 +1,16 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
-import { getUserByUsername } from '../../../../database/functions/user';
+import { getUserByUsername, validateAuthorization } from '../../../../database/functions/user';
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   const data: { authorization: string } = req.body;
-  const username = data.authorization.split('@')[0];
-  const apiKey = data.authorization.split('@')[1];
 
+  const isValid = await validateAuthorization(data.authorization);
+
+  const username = data.authorization.split('@')[0];
   const user = await getUserByUsername(username);
 
-  // If the user doesn't exist.
-  if (!user) {
-    res.status(401).end();
-    return;
-  }
-  // If the API key doesn't match.
-  if (apiKey !== user.apiKey) {
+  if (!user || !isValid) {
     res.status(401).end();
     return;
   }
