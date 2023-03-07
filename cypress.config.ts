427f921470eda16af7b28c9a291e52b9c372bd1f
Hunter Parcells <hunterparcells@gmail.com>
2023-03-07 00:17:53 -0500

    Increase timeout for Cypress tests.

--- cypress.config.ts
+++ cypress.config.ts
@@ -22,5 +22,10 @@ export default defineConfig({
     },
   },
   video: false,
-  screenshotOnRunFailure: false
+  screenshotOnRunFailure: false,
+  pageLoadTimeout: 100000,
+  requestTimeout: 100000,
+  responseTimeout: 100000,
+  defaultCommandTimeout: 100000,
+  taskTimeout: 100000
 });
